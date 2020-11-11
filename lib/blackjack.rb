34158304f4b84ecda0fa11a9b93b65c390a508fe
Joy Evans <joyevans@me.com>
2020-11-11 18:32:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,17 +42,16 @@ def hit?(user_hand)
   prompt_user
   user_input = get_user_input
   
-  if user_input != 'h' && user_input != 's'
-    invalid_command
-    prompt_user
-    user_input = get_user_input
-  end
-  
   if user_input == 'h'
     new_card = deal_card
     user_hand += new_card
   elsif user_input == 's'
     user_hand
+  end
+    if user_input != 'h' && user_input != 's'
+    invalid_command
+    prompt_user
+    user_input = get_user_input
   end
   user_hand
 end
